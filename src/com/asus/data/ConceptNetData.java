9725e7cf9346a239cdb0b80ed8f862ab06503b3e
JaydenLin <j5905653@gmail.com>
2013-11-19 01:23:09 +0800

    fix show concept net

--- src/com/asus/data/ConceptNetData.java
+++ src/com/asus/data/ConceptNetData.java
@@ -17,44 +17,49 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 public class ConceptNetData {
 
 	public static ConceptNetData instance;
 	private AsyncTaskResponse<String> delegate;
 	private Gson gson;
-	
+
 	public static ConceptNetData getInstance() {
 		if (instance == null) {
 			instance = new ConceptNetData();
 		}
 		return instance;
 	}
-	
-	private ConceptNetData(){
-		
+
+	private ConceptNetData() {
+
 	}
-	
-	public void searchConceptNet(String text,String surfaceText,AsyncTaskResponse<String> delegate){
-		this.delegate=delegate;
+
+	public void searchConceptNet(String text, String surfaceText,
+			AsyncTaskResponse<String> delegate) {
+		this.delegate = delegate;
+		new getConceptNetData().execute(text, surfaceText);
 	}
-	
-	class getConceptNetData extends AsyncTask<String, Void, String>{
+
+	class getConceptNetData extends AsyncTask<String, Void, String> {
 
 		@Override
 		protected String doInBackground(String... params) {
 			// TODO Auto-generated method stub
-			String text=params[0].trim();
-			String surfaceText=params[1].trim();
-			
+			String text = params[0].trim();
+			String surfaceText = params[1].trim();
+
 			HttpClient client = new DefaultHttpClient();
 			try {
-				HttpGet get = new HttpGet("http://conceptnet5.media.mit.edu/data/5.1/search?text="+text+"surfaceText="+surfaceText);
+				HttpGet get = new HttpGet(
+						"http://conceptnet5.media.mit.edu/data/5.1/search?text="
+								+ text + "&surfaceText=" + surfaceText);
 				HttpResponse response = client.execute(get);
 				HttpEntity resEntity = response.getEntity();
 				String result = EntityUtils.toString(resEntity);
-				
-				//Log.w(TAG, result);
+
+				// Log.w(TAG, result);
 				return result;
 			} catch (ClientProtocolException e) {
 				// TODO Auto-generated catch block
@@ -71,16 +76,15 @@ public class ConceptNetData {
 		protected void onPostExecute(String result) {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result);
+			String parsedResult="";
 			JsonElement jsonElement=new JsonParser().parse(result);
+			Log.w(getClass().getSimpleName(), result);
 			JsonObject jsonObject=jsonElement.getAsJsonObject();
-			String parsedResult=jsonObject.getAsJsonArray("edges").get(0).getAsJsonObject().get("surfaceText").toString();
+			if(jsonObject.getAsJsonArray("edges").size()>0){
+				parsedResult=jsonObject.getAsJsonArray("edges").get(0).getAsJsonObject().get("surfaceText").toString();
+			}
 			delegate.processFinish(parsedResult);
 		}
-		
-		
-		
 	}
-	
-	
-	
+
 }
